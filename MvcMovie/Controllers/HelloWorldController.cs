0de3787691c2b1a3d4d143221ac723bc6668f0d4
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-02 05:08:40 -0500

    Changed Welcome method in HelloWorldController.cs to add values to ViewData, which passes data to newly created Welcome.cshtml view template

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -5,35 +5,21 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // 
         // GET: /HelloWorld/
-
-        //public string Index()
-        //{
-        //    return "This is my default action...";
-        //}
 
         public IActionResult Index()
         {
             return View();
         }
 
-        // 
         // GET: /HelloWorld/Welcome/ 
 
-        //public string Welcome()
-        //{
-        //    return "This is the Welcome action method...";
-        //}
-
-        //public string Welcome(string name, int numTimes = 1)
-        //{
-        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
-        //}
-
-        public string Welcome(string name, int ID = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
