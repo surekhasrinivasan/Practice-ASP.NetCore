3a9061cfc687d36519d6f8b1963a5387f7231ddd
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-03 07:53:01 -0500

    Adding Search by genre - Modify the Index Method

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -19,13 +19,27 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        //public async Task<IActionResult> Index()
+        //public async Task<IActionResult> Index(string searchString)
         //{
-        //    return View(await _context.Movie.ToListAsync());
-        //}
+        //    var movies = from m in _context.Movie
+        //                 select m;
 
-        public async Task<IActionResult> Index(string searchString)
+        //    if (!String.IsNullOrEmpty(searchString))
+        //    {
+        //        movies = movies.Where(s => s.Title.Contains(searchString));
+        //    }
+
+        //    return View(await movies.ToListAsync());
+        //}
+        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
             var movies = from m in _context.Movie
                          select m;
 
@@ -34,8 +48,19 @@ namespace MvcMovie.Controllers
                 movies = movies.Where(s => s.Title.Contains(searchString));
             }
 
-            return View(await movies.ToListAsync());
+            if (!String.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            var movieGenreVM = new MovieGenreViewModel();
+            movieGenreVM.Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            movieGenreVM.Movies = await movies.ToListAsync();
+            movieGenreVM.SearchString = searchString;
+
+            return View(movieGenreVM);
         }
+
 
         // GET: Movies/Details/5
         public async Task<IActionResult> Details(int? id)
